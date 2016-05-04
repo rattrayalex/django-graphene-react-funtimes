6c9f30236e6edb5dca2bfe0b69d9b0c339b1ea52
Alex Rattray <alex@hired.com>
2016-05-03 22:33:22 -0700

    Initial Company and Job models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 
+
+class Company(models.Model):
+  name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+
+
+class Job(models.Model):
+  title = models.CharField(max_length=100)
+  description = models.TextField()
+  company = models.ForeignKey(Company, related_name='jobs')
+
+  def __str__(self):
+    return self.title
